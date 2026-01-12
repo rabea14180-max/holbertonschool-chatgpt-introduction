5cc52bc00d4210d0f4d8d803f969389103ed88b1
rabea14180-max <rabea14180@gmail.com>
2026-01-12 10:54:33 +0000

    Fix loop

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,11 +2,12 @@
 import sys
 
 def factorial(n):
-result = 1
-while n > 1:
-result *= n
-n -= 1
-return result
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
 
 f = factorial(int(sys.argv[1]))
 print(f)
+
